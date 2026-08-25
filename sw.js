9cf3f86b40fc2bf360ba394094b8a9d46662a745
iNxY5902 <1160252481@qq.com>
2026-08-25 14:54:56 +0800

    ?? PWA ??: ?? sw.js

---
+++ sw.js
@@ -0,0 +1,51 @@
+# Service Worker for PWA - 费用计算器
+const CACHE_NAME = 'fee-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// 安装时缓存资源
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache failed:', err))
+  );
+  self.skipWaiting();
+});
+
+// 激活时清理旧缓存
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 请求时优先使用缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/index.html');
+      })
+  );
+});
